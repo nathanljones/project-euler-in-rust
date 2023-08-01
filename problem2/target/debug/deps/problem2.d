0978c3d4d88a3cb5a4d79ed923a2f4229999358f
nathanljones <nathan.l.jones+github@protonmail.com>
2023-08-01 16:26:47 +0100

    Delete problem2/target directory

--- problem2/target/debug/deps/problem2.d
+++
@@ -1,5 +0,0 @@
-C:\project euler\rust_project-euler\problem2\target\debug\deps\problem2.exe: src\main.rs
-
-C:\project euler\rust_project-euler\problem2\target\debug\deps\problem2.d: src\main.rs
-
-src\main.rs:
--- problem2/target/debug/problem2.d
+++
@@ -1,1 +0,0 @@
-C:\project\ euler\rust_project-euler\problem2\target\debug\problem2.exe: C:\project\ euler\rust_project-euler\problem2\src\main.rs
